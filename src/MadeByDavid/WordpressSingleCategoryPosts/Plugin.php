eae11f187525f01c703b12158d4633132586e434
madebydavid <sutherland.dave@gmail.com>
2014-06-06 12:51:46 +0100

    quick version - swap checkboxes for radios

--- src/MadeByDavid/WordpressSingleCategoryPosts/Plugin.php
+++ src/MadeByDavid/WordpressSingleCategoryPosts/Plugin.php
@@ -9,11 +9,23 @@ class Plugin {
     function __construct() {
         
         add_action('init', array($this, 'init'), 0);
+        
+        add_action('admin_footer', array($this, 'changeCategoryCheckboxesToRadios'));
+        
             
     }
     
     public function init(){
         
     }    
-
+    
+    public function changeCategoryCheckboxesToRadios() {
+        echo <<<HTML
+            <script type="text/javascript">
+                jQuery("#categorychecklist input, #categorychecklist-pop input, .cat-checklist input").each(function(){this.type="radio"});
+            </script>
+HTML;
+        
+    }
+    
 }
